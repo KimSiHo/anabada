15a22d4b14c97d288315f7971f2971e37f7b5086
Kim SiHo <kim125y@naver.com>
2022-08-07 10:01:53 +0900

    수정
    수정

--- src/main/java/me/blog/anabada/service/impl/S3FileUploadService.java
+++ src/main/java/me/blog/anabada/service/impl/S3FileUploadService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
@@ -13,6 +14,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.blog.anabada.dto.file.S3Component;
+import me.blog.anabada.dto.file.UploadInfo;
 import me.blog.anabada.service.FileUploadService;
 
 @Profile("fs-s3")
@@ -31,5 +33,10 @@ public class S3FileUploadService implements FileUploadService {
 
     public String getFileUrl(String fileName) {
         return String.valueOf(amazonS3Client.getUrl(s3Component.getBucket(), fileName));
+    }
+
+    @Override
+    public UploadInfo saveFile(MultipartFile file) {
+        return null;
     }
 }
