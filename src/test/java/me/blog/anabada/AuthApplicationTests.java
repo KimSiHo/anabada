a166e7d01735e5aa5e133046b899e6bb203f6e3a
Kim SiHo <kim125y@naver.com>
2022-08-07 10:04:49 +0900

    Update AuthApplicationTests.java

--- src/test/java/me/blog/anabada/AuthApplicationTests.java
+++ src/test/java/me/blog/anabada/AuthApplicationTests.java
@@ -1,8 +1,17 @@
 package me.blog.anabada;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:/application-local.yml")
 @SpringBootTest
 class AuthApplicationTests {
 
